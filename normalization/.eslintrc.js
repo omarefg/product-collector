338bb99bda9457e5baa64d98c1a6e67d0a697fef
Omar Flores Grimontt <omar.flores@3dves.com>
2020-02-23 18:22:12 -0500

    added eslint and commit validation

---
+++ normalization/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    "indent": [2, 4],
+    "no-underscore-dangle": [2, { "allowAfterThis": true }]
+  }
+}
