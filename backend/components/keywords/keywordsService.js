861f4d8e9dc5d7a83b825aa9e3b821003b5e8810
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2020-03-10 11:41:32 -0600

    [Backend] finished the #72 and #73 issues Created the routes and services of keywords

--- backend/components/keywords/keywordsService.js
+++ backend/components/keywords/keywordsService.js
@@ -6,6 +6,27 @@ class KeywordsService {
     this.collection = 'keywords';
     this.mongoDB = new MongoLib();
   }
+  async getKeywords() {
+    const query = {};
+    const keywords = await this.mongoDB.getAll(this.collection, query);
+    return keywords || [];
+  }
+  async getKeyword({ keywordId }) {
+    const Keyword = await this.mongoDB.get(this.collection, keywordId);
+    return Keyword || {};
+  }
+  async createKeyword({ keyword }) {
+    const createKeywordId = await this.mongoDB.create(this.collection, keyword);
+    return createKeywordId;
+  }
+  async updateKeyword({ keywordId, keyword }) {
+    const updatedKeywordId = await this.mongoDB.update(this.collection, keywordId, keyword);
+    return updatedKeywordId;
+  }
+  async deleteKeyword({ keywordId }) {
+    const deletedKeywordId = await this.mongoDB.delete(this.collection, keywordId);
+    return deletedKeywordId;
+  }
 
 }
 module.exports = KeywordsService;
--- backend/components/keywords/routes.js
+++ backend/components/keywords/routes.js
@@ -1,10 +1,93 @@
 const express = require('express');
+const KeywordsService = require('./keywordsService');
 
 
 function keywordsApi(app) {
   const router = express.Router();
   app.use('/api/keywords', router);
+  const keywordsService = new KeywordsService();
 
+  router.get('/',async function(req, res, next) {
+    const { tags } = req.query;
+    try {
+      const keywords = await keywordsService.getKeywords(tags);
+      res.status(200).json({
+        data: keywords,
+        message: 'keywords listed'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.get(
+    '/:keywordId',
+    async function(req, res, next) {
+      const { keywordId } = req.params;
+      try {
+        const keywords = await keywordsService.getKeyword({ keywordId });
+
+        res.status(200).json({
+          data: keywords,
+          message: 'keywords retrieved'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+  router.post('/', 
+    async function(
+    req,
+    res,
+    next
+  ) {
+    const { body: keyword } = req;
+    try {
+      const createdKeywordId = await keywordsService.createKeyword({ keyword });
+
+      res.status(201).json({
+        data: createdKeywordId,
+        message: 'keyword created'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.patch(
+    '/:keywordId',
+    async function(req, res, next) {
+      const { keywordId } = req.params;
+      const { body: keyword } = req;
+      try {
+        const updatedKeywordId = await keywordsService.updateKeyword({
+          keywordId,
+          keyword
+        });
+
+        res.status(200).json({
+          data: updatedKeywordId,
+          message: 'keyword patched'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+  router.delete(
+    '/:keywordId',
+    async function(req, res, next) {
+      const { keywordId } = req.params;
+      try {
+        const deletedKeywordId = await keywordsService.deleteKeyword({ keywordId });
+        res.status(200).json({
+          data: deletedKeywordId,
+          message: 'keyword deleted'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
 
 }
 
