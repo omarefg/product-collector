dc324213042e046e60180e3de76dca1b3125a99f
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-04 19:25:59 -0500

    normalized ML data

--- normalization/components/data-manager/services.js
+++ normalization/components/data-manager/services.js
@@ -26,11 +26,10 @@ class DataManagerService {
 
     async normalize (dataReqBody) {
         const { id, source, data, date } = dataReqBody
-        const criteria = productSearchCriteria[id]
         switch (source) {
         case 'PCML': {
             try {
-                return this.normalizeFromML(criteria, id, source, data, date)
+                return this.normalizeFromML(id, source, data, date)
             } catch (error) {
                 throw boom.badRequest(error.message)
             }
@@ -39,20 +38,22 @@ class DataManagerService {
         }
     }
 
-    async normalizeFromML (criteria, id, source, data, date) {
-        // const dataToResponse = this.reduceByFiltersFromML(data, criteria.filters)
-        let dataToResponse = this._simplifyDataFromML(id, source, data, date)
-        dataToResponse = this._getVariantFromML(dataToResponse, criteria.filters.variants)
-        return dataToResponse
+    async normalizeFromML (id, source, data, date) {
+        const criteria = productSearchCriteria[id]
+        let normalizedData = await this._simplifyDataFromML(id, source, data, date)
+        normalizedData = this._getVariantFromML(normalizedData, criteria.filters.variants)
+        normalizedData = this._applyCriteriaForML(normalizedData, criteria.filters)
+        return normalizedData
     }
 
-    _simplifyDataFromML (id, source, data, date) {
+    async _simplifyDataFromML (id, source, data, date) {
         try {
             const results = data.results.map(item => ({
                 title: item.title.toLowerCase(),
-                // countryState: item.address.state_name.toLowerCase(),
+                countryState: item.address.state_name,
                 currency: item.currency_id.toLowerCase(),
                 condition: item.attributes.filter(item => item.id === 'ITEM_CONDITION')[0],
+                conditions: item.attributes.filter(item => item.id === 'ITEM_CONDITION')[0],
                 model: item.attributes.filter(item => item.id === 'MODEL')[0],
                 price: item.price,
                 soldQuantity: item.sold_quantity,
@@ -65,8 +66,10 @@ class DataManagerService {
                 country: countries[source].data.filter(item => item.id === data.site_id)[0].name,
                 results: results.map(item => ({
                     ...item,
-                    condition: item.condition ? item.condition.value_name.toLowerCase() : 'undefined',
-                    model: item.model ? item.model.value_name.toLowerCase() : 'undefined'
+                    condition: item.condition ? item.condition.value_name.toLowerCase() : '',
+                    conditions: item.condition ? item.condition.value_name.toLowerCase() : '',
+                    model: item.model ? item.model.value_name.toLowerCase() : '',
+                    models: item.model ? item.model.value_name.toLowerCase() : ''
                 }))
             }
         } catch (error) {
@@ -78,14 +81,35 @@ class DataManagerService {
         const regExpVariants = new RegExp(optionsToFind.join('|'))
         return {
             ...data,
-            results:
-            data.results
+            results: data.results
                 .filter(item => regExpVariants.test(item.title))
-                .map(item => ({
-                    ...item,
-                    variant: item.title.match(regExpVariants)[0],
-                    title: undefined
-                }))
+                .map(item => {
+                    const itemObj = {
+                        ...item,
+                        variant: item.title.match(regExpVariants)[0],
+                        variants: item.title.match(regExpVariants)[0]
+                    }
+                    delete itemObj.title
+                    return itemObj
+                })
+        }
+    }
+
+    _applyCriteriaForML (data, filters) {
+        const keys = Object.keys(filters)
+        filters.conditions = ['nuevo', 'usado', 'reacondicionado']
+        return {
+            ...data,
+            results: data.results.filter(item => {
+                let itsGoingToBeReturned = true
+                for (let i = 0; i < keys.length; i++) {
+                    if (filters[keys[i]].length > 0 && !filters[keys[i]].includes(item[keys[i]])) {
+                        itsGoingToBeReturned = false
+                    }
+                    delete item[keys[i]]
+                }
+                return itsGoingToBeReturned
+            })
         }
     }
 }
