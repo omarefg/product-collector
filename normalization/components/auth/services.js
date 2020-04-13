bd2a2a0d968b965bfce926145fcb2e86bbaea822
Andrés Santana <drew.lizcano@gmail.com>
2020-04-13 12:51:18 -0500

    Documentation

--- normalization/components/auth/services.js
+++ normalization/components/auth/services.js
@@ -1,40 +1,46 @@
-const boom = require('@hapi/boom')
-const jwt = require('jsonwebtoken')
+const boom = require("@hapi/boom");
+const jwt = require("jsonwebtoken");
 
-const RedisLib = require('../../lib/RedisLib')
-const { generateRandomHex, isOkDecrypt, encryptDynamicHash } = require('../../utils/auxToken')
-const { authKey } = require('../../config/index').config
+const RedisLib = require("../../lib/RedisLib");
+const {
+  generateRandomHex,
+  isOkDecrypt,
+  encryptDynamicHash,
+} = require("../../utils/auxToken");
+const { authKey } = require("../../config/index").config;
 
-const TWO_HOURS_IN_SEC = 7200
+const TWO_HOURS_IN_SEC = 7200;
 
 class AuthService {
-    constructor () {
-        this.redisDB = new RedisLib(TWO_HOURS_IN_SEC)
+  constructor() {
+    this.redisDB = new RedisLib(TWO_HOURS_IN_SEC);
+  }
+
+  async validateKey(authKeyHashed) {
+    const authKeyOk = await isOkDecrypt(authKey, authKeyHashed);
+    if (!authKeyOk) {
+      throw boom.unauthorized();
     }
 
-    async validateKey (authKeyHashed) {
-        const authKeyOk = await isOkDecrypt(authKey, authKeyHashed)
-        if (!authKeyOk) {
-            throw boom.unauthorized()
-        }
+    const secret = generateRandomHex();
+    const token = jwt.sign({ key: authKey }, secret, {
+      expiresIn: TWO_HOURS_IN_SEC,
+    });
 
-        const secret = generateRandomHex()
-        const token = jwt.sign({ key: authKey }, secret, { expiresIn: TWO_HOURS_IN_SEC })
+    const tokenSaved = await this.redisDB.set(token, secret);
 
-        const tokenSaved = await this.redisDB.set(token, secret)
-
-        if (tokenSaved === true) {
-            return token
-        } else {
-            throw boom.internal(tokenSaved)
-        }
+    if (tokenSaved === true) {
+      return token;
+    } else {
+      throw boom.internal(tokenSaved);
     }
+  }
 
-    async encryptData (dataToEncrypt) {
-        const { data, rounds } = dataToEncrypt
-        const encryptedData = await encryptDynamicHash(data, rounds)
-        return encryptedData
-    }
+  async encryptData(dataToEncrypt) {
+    const { data, rounds } = dataToEncrypt;
+    const encryptedData = await encryptDynamicHash(data, rounds);
+    return encryptedData;
+  }
 }
 
-module.exports = AuthService
+module.exports = AuthService;
